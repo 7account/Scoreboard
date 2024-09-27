2afb9eaf73ac59e6b3cb8f78399ccb47a5618720
7account <DAvidliancongc2789@nycstudents.net>
2024-09-27 13:29:53 +0000

    update 1 (constructor and 2 methods)

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
     }
 }
